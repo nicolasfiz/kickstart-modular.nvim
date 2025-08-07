a9daab12c13f5a57cc99fdbe9d787d9196c36e0c
nicolasfiz <nicolasfiz@outlook.com>
2025-08-07 19:32:59 +0200

    add gh copilot

---
+++ lua/custom/plugins/copilot.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup {
+        filetypes = {
+          ['*'] = true,
+        },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          keymap = {
+            accept = '<Tab>',
+          },
+        },
+      }
+    end,
+  },
+  {
+    'saghen/blink.cmp',
+    optional = true,
+    dependencies = { 'fang2hou/blink-copilot' },
+    opts = {
+      sources = {
+        default = { 'copilot' },
+        providers = {
+          copilot = {
+            name = 'copilot',
+            module = 'blink-copilot',
+            score_offset = 100,
+            async = true,
+          },
+        },
+      },
+    },
+  },
+}
